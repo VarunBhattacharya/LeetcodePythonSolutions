e0665b4e77cd9451660515da22fbb65f2b62e231
VarunBhattacharya <76221455+VarunBhattacharya@users.noreply.github.com>
2022-12-23 12:35:18 +0530

    initial commit

---
+++ 69_Sqrt_x.py
@@ -0,0 +1,31 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        #Naive Solution
+        '''
+        return int(pow(x,0.5))
+        '''
+
+        #Better Solution - Binary Search - O(logN) solution
+        '''
+        left, right, mid = 0, x, 0
+        while left <= right:
+            mid = (left+right)//2
+            if mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                return mid
+
+        return right
+        '''
+
+        #Better Solution - Newton Method - O(logN) solution
+        res = x
+        while not res * res - x < 1:
+            res = (res + x / res)/2
+        return int(res)
+
+if __name__ == '__main__':
+    x = 8
+    print(Solution().mySqrt(x))
