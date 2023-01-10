7775a26fa895e2a449e1b928528bc3bd5a0e215a
VarunBhattacharya <76221455+VarunBhattacharya@users.noreply.github.com>
2023-01-10 21:55:25 +0530

    initial commit

---
+++ 145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root):
+        #Recursive Approach
+        res = []
+        def postOrder(root):
+            if not root:
+                return
+            else:
+                postOrder(root.right)
+                postOrder(root.left)
+                res.append(root.val)
+        postOrder(root)
+        return res
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    root.right = TreeNode(2)
+    root.right.left = TreeNode(3)
+    print(Solution().postorderTraversal(root))
