d62e8c082501a150d6b9d0ee397ac1b1f077deed
VarunBhattacharya <76221455+VarunBhattacharya@users.noreply.github.com>
2023-06-07 16:59:56 +0530

    extra approach added

--- 605_Can_Place_Flowers.py
+++ 605_Can_Place_Flowers.py
@@ -1,6 +1,7 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed, n: int) -> bool:
         #Approach 1: Intuitive Method - Time Complexity - O(N) and Space Complexity - O(N)
+        '''
         flowerbedMod = [0] + flowerbed + [0]
 
         for i in range(1, len(flowerbedMod)-1):
@@ -8,6 +9,7 @@ class Solution:
                 flowerbedMod[i] = 1
                 n -= 1
         return n <= 0
+        '''
 
         #Approach 2: Better Space Complexity - Time Complexity - O(N) and Space Complexity - O(1)
         '''
@@ -23,6 +25,16 @@ class Solution:
         return n <= 0
         '''
 
+        #Naive Approach: Time Complexity - O(N) and Space Complexity - O(1)
+        i = 0
+        while i < len(flowerbed) and n > 0:
+            if flowerbed[i] == 0:
+                if i == 0 or flowerbed[i-1] == 0:
+                    if i == len(flowerbed)-1 or flowerbed[i+1] == 0:
+                        flowerbed[i] = 1
+                        n -= 1
+            i += 1
+        return n <= 0
 
 if __name__ == '__main__':
     print(Solution().canPlaceFlowers([1,0,0,0,1],1))
