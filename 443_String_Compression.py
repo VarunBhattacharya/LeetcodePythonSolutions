22dfdb9b798d4ac7d7473d822164c2245093f417
VarunBhattacharya <varun.bhattacharya2020@vitstudent.ac.in>
2023-06-11 12:43:57 +0530

    added approaches

--- 443_String_Compression.py
+++ 443_String_Compression.py
@@ -1,25 +1,25 @@
-'''
-Given an array of characters chars, compress it using the following algorithm:
-
-Begin with an empty string s. For each group of consecutive repeating characters in chars:
-
-    If the group's length is 1, append the character to s.
-    Otherwise, append the character followed by the group's length.
-
-The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
-
-After you are done modifying the input array, return the new length of the array.
-
-You must write an algorithm that uses only constant extra space.
-
-Example 1:
-chars = ["a","a","b","b","c","c","c"]
-Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
-'''
-
 class Solution:
     def compress(self, chars) -> int:
-        #Naive Approach: Two Pointer - Time Complexity - O(N) and Space Complexity - O(1)
+        #Approach 1: Time Complexity - O(N^2) and Space Complexity - O(N)
+        '''
+        if not chars:
+            return 0
+        res = []
+        pnt1 = 0
+        while pnt1 < len(chars):
+            pnt2 = pnt1 + 1
+            while pnt2 < len(chars) and chars[pnt1] == chars[pnt2]:
+                pnt2 += 1
+            res.append(chars[pnt1])
+            if pnt2 - pnt1 > 1:
+                res += list(str(pnt2 - pnt1))
+            pnt1 = pnt2
+        chars[:] = res
+        return len(chars)
+        '''
+
+        #Approach 2: Two Pointer - Time Complexity - O(N) and Space Complexity - O(1)
+        '''
         if not chars:
             return 0
         initialChar = chars[0]
@@ -40,7 +40,29 @@ class Solution:
                 initialChar = popChar
                 skips = 1
         return len(chars)
+        '''
+
+        #Approach 3: Two Pointer - Time Complexity - O(N) and Space Complexity - O(1)
+        if len(chars) < 2:
+            return len(chars)
+        pnt1, pnt2 = 0, 0
+        while pnt1 < len(chars):
+            tempCnt = 1
+            while pnt1 < len(chars) - 1 and chars[pnt1] == chars[pnt1+1]:
+                tempCnt += 1
+                pnt1 += 1
+            
+            chars[pnt2] = chars[pnt1]
+            pnt2 += 1
+            if tempCnt > 1:
+                for num in str(tempCnt):
+                    chars[pnt2] = str(num)
+                    pnt2 += 1
+
+            pnt1 += 1
+        return pnt2
     
 if __name__ == "__main__":
     sol = Solution()
     print(sol.compress(["a","a","b","b","c","c","c"]))
+    print(sol.compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"]))
